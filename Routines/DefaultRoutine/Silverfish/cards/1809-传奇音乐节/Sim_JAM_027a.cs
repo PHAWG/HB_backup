82cc3e089faf999dc6ed610278da0aed45bb1f50
xiaohei7972 <1901810053@qq.com>
2026-05-04 02:30:11 +0800

    更新几张sim

--- Routines/DefaultRoutine/Silverfish/cards/1809-传奇音乐节/Sim_JAM_027a.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1809-传奇音乐节/Sim_JAM_027a.cs
@@ -11,7 +11,24 @@ namespace HREngine.Bots
 	//使一个友方随从获得+2攻击力和<b>突袭</b>。
 	class Sim_JAM_027a : SimTemplate
 	{
-		
-		
+		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice, Handmanager.Handcard hc)
+		{
+			if (target != null)
+			{
+
+				p.minionGetBuffed(target, 2, 0);
+				p.minionGetRush(target);
+			}
+		}
+
+		public override PlayReq[] GetPlayReqs()
+		{
+			return new PlayReq[]
+			{
+				new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE),
+				new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET),
+				new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET),
+			};
+		}
 	}
 }
--- Routines/DefaultRoutine/Silverfish/cards/1809-传奇音乐节/Sim_JAM_027b.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1809-传奇音乐节/Sim_JAM_027b.cs
@@ -11,7 +11,22 @@ namespace HREngine.Bots
 	//使一个友方随从获得+2生命值和<b>吸血</b>。
 	class Sim_JAM_027b : SimTemplate
 	{
-		
-		
+		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice, Handmanager.Handcard hc)
+		{
+			if (target != null)
+			{
+				p.minionGetBuffed(target, 0, 2);
+				p.minionGetLifesteal(target);
+			}
+		}
+		public override PlayReq[] GetPlayReqs()
+		{
+			return new PlayReq[]
+			{
+				new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE),
+				new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET),
+				new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET),
+			};
+		}
 	}
 }
